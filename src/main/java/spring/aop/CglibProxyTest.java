0d0332399f28bd8b7f7c89eda199c70a9c9d8c13
EverythingStudy <759499199@qq.com>
2020-04-02 18:16:02 +0800

    Spring CglibProxy

--- src/main/java/spring/aop/CglibProxyTest.java
+++ src/main/java/spring/aop/CglibProxyTest.java
@@ -1,15 +1,17 @@
 package spring.aop;
 
 import org.junit.Test;
+import org.springframework.cglib.core.DebuggingClassWriter;
 
 /**
- * @Description
+ * @Description Cglib测试
  * @Date 2020/3/25 11:36
  * @Author cly
  **/
 public class CglibProxyTest {
     @Test
     public void test(){
+        System.out.println();
         CglibProxy cglibProxy=new CglibProxy();
         TargetInterface targetInterface=(TargetInterface)cglibProxy.getProxy(TargetInterfaceImpl.class);
         targetInterface.removeTopic(10);
